ddbf891068841c2adbc42f4ffe4fc7f8ec137f33
philliphall131 <phillip.hall131@gmail.com>
2022-06-30 20:50:03 -0400

    DRF and react initialized

--- backend/iron_app/admin.py
+++ backend/iron_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+class AppUserAdmin(UserAdmin):
+    """adds some additional attributes to the default edit user page in django admin"""
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email","first_name", "last_name", "password1", "password2"),
+            },
+        ),
+    )
+
+admin.site.register(User, AppUserAdmin)
--- backend/iron_app/views.py
+++ backend/iron_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
+from .models import *
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .views_auth import *
+from rest_framework.authtoken.models import Token
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            permission_classes = [AllowAny]
+        else: 
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        token = Token.objects.create(user=serializer.instance)
+        return_data = { 'user': serializer.data, 'token': token.key }
+        return Response(return_data, status=status.HTTP_201_CREATED, headers=headers)
