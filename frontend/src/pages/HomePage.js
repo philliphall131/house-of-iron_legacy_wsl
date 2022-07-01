3ecd5ef7e4c6df91cb5688ee0d9c5a84b477435e
philliphall131 <phillip.hall131@gmail.com>
2022-06-30 21:03:07 -0400

    initialized react project with homepage

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>New Home Page</h1>
+      </header>
+    </div>
+  )
+}
+
+export default HomePage
