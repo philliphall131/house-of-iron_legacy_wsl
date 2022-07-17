81751ba1b80e1bdc9cd61d845a5044b8a279339e
philliphall131 <phillip.hall131@gmail.com>
2022-07-16 21:42:30 -0400

    added dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,14 +1,36 @@
-import { useContext } from 'react';
-import '../styles/App.css';
+import { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Dashboard.css';
 import { StateContext } from '../ContextObjs';
 
-const Dashboard = ({name}) => {
+
+const Dashboard = () => {
   const { state } = useContext(StateContext);
+
+  useEffect(()=>{
+    loadPrograms()
+  }, [])
+
+  const loadPrograms = () => {
+    return
+  }
+
   return (
-    <div className="standard-body">
-      <div className="main-content">
-        <h1>Dashboard</h1>
-        <p>{state.user.id}</p>
+    <div className="dash-body">
+      <div className="dash-content">
+        <h1 className="dash-title">HAVE YOU LIFTED TODAY, {state.user.first_name}?</h1>
+        <Link to="/dashboard">
+          <button className="dash-button">Workout Schedule</button>
+        </Link>
+        <Link to="/dashboard">
+          <button className="dash-button">Create/Edit Programs</button>
+        </Link>
+        <Link to="/dashboard">
+          <button className="dash-button">History/Maxes</button>
+        </Link>
+        <Link to="/dashboard">
+          <button className="dash-button">Account</button>
+        </Link>
       </div>
     </div>
   )
